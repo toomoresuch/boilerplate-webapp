ec9ac7ca21086741a74420eda95aa4cab10478c9
AYUkawa,Yasuyuki <github@yamayo.to>
2013-05-15 12:10:05 +0900

    Activate smusher

--- config.rb
+++ config.rb
@@ -47,13 +47,16 @@
 #   end
 # end
 
+set :build_dir, 'theme'
 set :css_dir, 'css'
-
 set :images_dir, 'img'
-
 set :js_dir, 'js'
 
 activate :livereload
+
+ignore '*.md'
+ignore 'doc/*'
+ignore 'sozai/*'
 
 after_configuration do
   sprockets.append_path "#{root}/components"
@@ -62,10 +65,10 @@ end
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
@@ -75,8 +78,8 @@ configure :build do
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
+  require "middleman-smusher"
+  activate :smusher
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
